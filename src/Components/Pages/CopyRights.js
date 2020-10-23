5a29908636402ffd0bb0dd531ab1ca55643d60cf
95lux <jlx@235media.com>
2020-10-23 15:34:26 +0200

    add version in app

--- src/Components/Pages/CopyRights.js
+++ src/Components/Pages/CopyRights.js
@@ -3,17 +3,27 @@ import React from 'react';
 import '../../styles/App.css';
 
 
+
 // here app catches the suspense from page in case translations are not yet loaded
 export default function CopyRights() {
+
+console.log();
 
     return (
         <div className='page'>
             <div style={blackStyle}>
                 <img src='/bilder/copyrights.jpg' style={imgStyle}></img>
-
+                <div style={versionStyle}>v{process.env.REACT_APP_VERSION}</div>
             </div>
         </div>
     );
+}
+
+const versionStyle = {
+    position: 'absolute',
+    bottom: '0',
+    right: '0'
+
 }
 
 const imgStyle = {
